b393dfc632acbe2db83b3400e722f2c60d67ccc1
mrochelle23 <mrochellebiz@gmail.com>
2025-04-13 19:54:19 -0400

    added two other items

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -27,6 +27,20 @@ function App() {
       image: '/images/toy2.png',
       description: 'The SolarMorph 12X Builder Kit is an eco-friendly robotics set that lets kids create 12 unique solar-powered models using over 190 interchangeable pieces. From crawling crabs to rolling rovers and soaring windmills, each design comes to life using solar energy—no batteries required! It’s the ultimate introduction to green technology and mechanical engineering, designed to spark innovation and environmental awareness in young inventors.',
     },
+    {
+      id: 3,
+      name: 'InventorBox Explorer',
+      price: 45.99,
+      image: '/images/toy3.png',
+      description: "Unleash your inner genius with the InventorBox Explorer. Packed with over 700 top-secret components (okay, maybe just cool wires, gears, and googly eyes), this kit is your launchpad to invent wild gadgets, decode imaginary messages, and construct your very own squirrel surveillance drone (batteries not included, squirrels not guaranteed). Whether you're building a snack catapult for stealth missions or a rubber band-powered rocket to Mars, the only limit is your imagination (and maybe the laws of physics). Ideal for ages 8 to 88, this box is the official toolkit of future spies, eccentric geniuses, and cereal-box engineers everywhere.",
+    },
+    {
+      id: 4,
+      name: 'Earth Science Volcano Builder Kit',
+      price: 75.99,
+      image: '/images/toy4.png',
+      description: 'Dive headfirst into the explosive (and slightly messy) world of Earth Science with the Earth Science Volcano Builder Kit. This kit contains everything an aspiring geologist-slash-volcano-wrangler needs to survive in the wild—or at least dominate the next school science fair.',
+    },
   ];
 
   return (
